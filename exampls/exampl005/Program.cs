c4158ec65fae7964b7e9c433e5cfffe36383280c
albertboronovskij <albertikus1010@gmail.com>
2023-02-25 17:07:20 +0700

    сделали диалог

---
+++ exampls/exampl005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("назови своё имя сынок ");
+string username = Console.ReadLine();
+
+if (username == "ахилес")
+{
+    Console.WriteLine("сын пелея");
+}
+else
+{
+    Console.Write("ты не ");
+    Console.Write(username);
+    Console.Write(" ты пердунья");
+}
